9c76679fc2a2b59703d4a38988bfa78ab88e701b
Your Name <dariolopes2001@gmail.com>
2025-12-26 00:16:10 -0300

    atualizando conexao com o db

--- db.js
+++ db.js
@@ -4,6 +4,8 @@ export async function connectDB() {
   const uri = process.env.MONGO_URI;
   if (!uri) throw new Error("MONGO_URI não definido nas variáveis de ambiente");
 
-  await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  // await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  await mongoose.connect(uri);
+
   console.log("✅ Conectado ao MongoDB Atlas");
 }
